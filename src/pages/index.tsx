001289bf8fb3ccecc8173bc1f378a6f7b4119c63
vxfontes <nessa1vane@icloud.com>
2023-05-12 18:38:09 -0300

    feat: adicionando verificação de login nas paginas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import Financas from "@/components/financas";
 import Landing from "@/components/landing";
+import Carregando from "@/components/template/Carregando";
+import AutenticacaoContext from "@/data/contexts/AutenticacaoContext";
+import { useContext } from "react";
 
 export default function Home() {
-  return (
-    <>
-      <Landing />
-      {/* <Financas /> */}
-    </>
-  )
+    const { usuario, carregando } = useContext(AutenticacaoContext)
+
+    // verificando se a aplicação esta ou não carregando
+    if (carregando) return <Carregando />
+    return usuario ? <Financas /> : <Landing />
 }
