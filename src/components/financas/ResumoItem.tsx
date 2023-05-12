aee6033fd5502e829e3c402a1d6628a295a56454
vxfontes <nessa1vane@icloud.com>
2023-05-12 09:12:18 -0300

    feat: resumo de receitas e gastos

---
+++ src/components/financas/ResumoItem.tsx
@@ -0,0 +1,36 @@
+import Dinheiro from "@/logic/utils/Dinheiro"
+import React from "react"
+
+
+export interface ResumoItemProps {
+    titulo: string
+    valor: number
+    icone: any
+    valorClassName?: string
+    iconeClassName?: string
+}
+
+export default function ResumoItem(props: ResumoItemProps) {
+    return (
+        <div className={`
+            relative flex flex-col bg-black
+            pt-4 pb-3 px-5 rounded-lg
+            border border-zinc-800 text-white
+        `}>
+            <div className="text-sm text-zinc-500">{props.titulo}</div>
+            <div className="flex justify-between items-center">
+                <span className={`text-3xl font-black ${props.valorClassName ?? ''}`}>
+                    {Dinheiro.formatar(props.valor)}
+                </span>
+                <span>
+                    {/* clonando o icone */}
+                    {React.cloneElement(props.icone, {
+                        size: 60,
+                        strokeWidth: 1,
+                        className: `${props.iconeClassName ?? ''}`,
+                    })}
+                </span>
+            </div>
+        </div>
+    )
+}
