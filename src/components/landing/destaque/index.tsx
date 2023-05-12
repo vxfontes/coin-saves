0d85e026c03537a077820324507a6d19ef91c6b1
vxfontes <nessa1vane@icloud.com>
2023-05-12 08:37:44 -0300

    feat: finalizando landing page

---
+++ src/components/landing/destaque/index.tsx
@@ -0,0 +1,23 @@
+import Area from "../comum/Area";
+import Slogan from "./Slogan";
+import principal from "../../../assets/principal.jpg"
+import ImagemResponsiva from "../comum/ImagemResponsiva";
+
+// escondemos a imagem se o dispositivo por muito pequeno
+// o pt-20 empurra o rodape para baixo, ficando sempre no footer da pagina
+export default function Destaque() {
+    return (
+        <Area className="pt-20">
+            <div className={`
+                flex items-center justify-around
+                h-[500px]
+            `}>
+                <Slogan />
+                <ImagemResponsiva
+                    imagem={principal}
+                    className="rotate-3 hidden md:inline"
+                />
+            </div>
+        </Area>
+    )
+}
