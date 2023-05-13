855b8e05f62781a861348073d1063da6ee8a0adb
vxfontes <nessa1vane@icloud.com>
2023-05-13 04:33:59 -0300

    feat: usando components do mantine

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { AutenticacaoProvider } from '@/data/contexts/AutenticacaoContext'
 import '@/styles/globals.css'
+import { MantineProvider } from '@mantine/core'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <AutenticacaoProvider>
-            <Component {...pageProps} />
-        </AutenticacaoProvider>
+        <MantineProvider theme={{ colorScheme: 'dark' }}>
+            <AutenticacaoProvider>
+                <Component {...pageProps} />
+            </AutenticacaoProvider>
+        </MantineProvider>
     )
 }
