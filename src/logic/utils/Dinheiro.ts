83a094e2b392caac8777aa2a0317a02bbc16df4d
vxfontes <nessa1vane@icloud.com>
2023-05-13 05:06:18 -0300

    feat: função de desformatar

--- src/logic/utils/Dinheiro.ts
+++ src/logic/utils/Dinheiro.ts
@@ -2,9 +2,25 @@ export default class Dinheiro {
     private static _lingua = "pt-BR"
     private static _moeda = "BRL"
 
+    /**
+     * 
+     * @param num recebe um numero
+     * @returns retorna o valor em dinheiro formatado por localização
+     */
     static formatar(num: number): string {
         return (num ?? 0).toLocaleString(Dinheiro._lingua, {
             style: "currency", currency: Dinheiro._moeda
         })
+    }
+
+    /**
+     * formatação numerica
+     * @param valor recebe o valor inteiro do dinheiro
+     * @returns retorna apenas um valor numerico do string enviado
+     */
+    static desformatar(valor: string): number {
+        const nums = valor.replace(/[^0-9]+/g, "")
+        const i = nums.length - 2
+        return Number(`${nums.substring(0, i)}.${nums.substring(i)}`)
     }
 }
