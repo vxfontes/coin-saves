fab54eb3e2b279f574f8dc8cd224c590997037c8
vxfontes <nessa1vane@icloud.com>
2023-05-13 04:33:44 -0300

    feat: menu do usuario

--- src/components/template/InfoUsuario.tsx
+++ src/components/template/InfoUsuario.tsx
@@ -1,4 +1,7 @@
 import AutenticacaoContext from "@/data/contexts/AutenticacaoContext";
+import { Avatar, Menu } from "@mantine/core";
+import { IconArrowsRightLeft, IconLogout, IconUser } from "@tabler/icons-react";
+import Link from "next/link";
 import { useContext } from "react";
 
 export default function InfoUsuario() {
@@ -6,15 +9,39 @@ export default function InfoUsuario() {
     const { usuario, logout } = useContext(AutenticacaoContext)
 
     return (
-        <div className='flex items-center gap-3 cursor-pointer' onClick={logout}>
-            <div className='flex flex-col select-none text-right'>
-                <span className='text-sm font-bold text-zinc-200'>
-                    {usuario?.nome}
-                </span>
-                <span className='text-xs text-zinc-400'>
-                    {usuario?.email}
-                </span>
-            </div>
-        </div>
+        <Menu>
+            <Menu.Target>
+                <div className='flex items-center gap-3 cursor-pointer'>
+                    <div className='hidden md:flex flex-col select-none'>
+                        <span className='text-sm font-bold text-zinc-200'>{usuario?.nome}</span>
+                        <span className='text-xs text-zinc-400'>{usuario?.email}</span>
+                    </div>
+                    <Avatar
+                        size={40}
+                        radius="xl"
+                        src={usuario?.avatar}
+                    />
+                </div>
+            </Menu.Target>
+            <Menu.Dropdown>
+                <Menu.Label>Usuário</Menu.Label>
+                <Link href="/">
+                    <Menu.Item
+                        icon={<IconArrowsRightLeft size={14} />}
+                    >Finanças</Menu.Item>
+                </Link>
+                <Link href="/usuario">
+                    <Menu.Item
+                        icon={<IconUser size={14} />}
+                    >Meus Dados</Menu.Item>
+                </Link>
+                <Menu.Divider />
+                <Menu.Item
+                    color="red"
+                    icon={<IconLogout size={14} />}
+                    onClick={logout}
+                >Sair do Sistema</Menu.Item>
+            </Menu.Dropdown>
+        </Menu>
     )
 }
