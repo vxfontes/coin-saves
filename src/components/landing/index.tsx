0c63cb8ace398609477564a06262eba0c627d995
vxfontes <nessa1vane@icloud.com>
2023-05-13 04:10:02 -0300

    feat: vantagens na landing page

--- src/components/landing/index.tsx
+++ src/components/landing/index.tsx
@@ -2,12 +2,14 @@ import Pagina from "../template/Pagina";
 import Cabecalho from "./cabecalho";
 import Destaque from "./destaque";
 import Rodape from "./rodape";
+import Vantagens from "./vantagens";
 
 export default function Landing() {
     return (
         <Pagina externa>
             <Cabecalho />
             <Destaque />
+            <Vantagens />
             <Rodape />
         </Pagina>
     )
