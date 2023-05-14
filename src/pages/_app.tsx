23cd2013c395040685e1591ff19496c636143c12
vxfontes <nessa1vane@icloud.com>
2023-05-13 23:37:24 -0300

    feat: adicionando titulo e icones

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,16 @@ import { AutenticacaoProvider } from '@/data/contexts/AutenticacaoContext'
 import '@/styles/globals.css'
 import { MantineProvider } from '@mantine/core'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <MantineProvider theme={{ colorScheme: 'dark' }}>
+            <Head>
+                <link rel="icon" href="/money-bag.png" />
+                <link rel="apple-touch-icon" href="/money-bag.png" />
+                <title>Coin Save</title>
+            </Head>
             <AutenticacaoProvider>
                 <Component {...pageProps} />
             </AutenticacaoProvider>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,9 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/money-bag.png" />
-        <link rel="apple-touch-icon" href="/money-bag.png" />
-        <title>Get My Money</title>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
