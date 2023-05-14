828b94bd0d11cf72b9787594602cd25d1d50691f
vxfontes <nessa1vane@icloud.com>
2023-05-13 23:08:05 -0300

    fix: consertando icone da aplicação

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head title='Get My Money' />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/money-bag.png" />
+        <link rel="apple-touch-icon" href="/money-bag.png" />
+        <title>Get My Money</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
