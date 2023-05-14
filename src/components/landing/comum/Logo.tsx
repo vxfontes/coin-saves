7f7f6444e136af5393fd4f800c5a3aab50b875ae
vxfontes <nessa1vane@icloud.com>
2023-05-13 23:13:37 -0300

    feat: mudando o nome da aplicação

--- src/components/landing/comum/Logo.tsx
+++ src/components/landing/comum/Logo.tsx
@@ -1,8 +1,8 @@
 export default function Logo() {
     return (
         <div className="text-3xl">
-            <span className="font-black">Get my</span>
-            <span className="text-zinc-400 font-thin"> MONEY</span>
+            <span className="font-black">Coin</span>
+            <span className="text-zinc-400 font-thin">SAVE</span>
         </div>
     )
 }
